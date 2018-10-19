380adc46cf7878cd1d044f9c108d3ac8519a1eed
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2018-10-19 10:37:54 +0200

    Le nom de la variable "length_1" ?

--- OtherSort.c
+++ OtherSort.c
@@ -14,12 +14,11 @@ void sort(int*t, size_t length){
 	}
 
 	if(length >= 3){
-      bool notMultiple = ((length%3)!=0);
-		size_t length_1 = (length/3) + notMultiple;
-		size_t length_2 = (2*length/3) + notMultiple - length_1;
+      bool notMultiple = ((length%3)!= 0);
+		size_t length_2 = (2*length/3) + notMultiple;
 
-		sort(t, length_1 + length_2);
-		sort(t+(length_1), length - length_1);
-      sort(t, length_1 + length_2);
+		sort(t, length_2);
+		sort(t+(length_2/2), length - (length_2/2));
+      sort(t, length_2);
 	}
 }
