68c9e12cf37496cce85d3317e7f44682862ceb08
Aurelienbernard <aurelien.bernard@student.uliege.be>
2018-10-20 23:34:40 +0200

    code cleaned and correctly commented

--- MergeSort.c
+++ MergeSort.c
@@ -1,46 +1,69 @@
 /* ========================================================================= *
- * MergeSort
- * Implementation of the MergeSort algorithm.
+ * mergeSort
+ * Implementation of the mergeSort algorithm.
  * ========================================================================= */
 
 #include <stdlib.h>
 #include "Sort.h"
 
-static void Merge(int* array, int* tmp_array, size_t start, size_t middle, size_t end ){
+/* -------------------------------------------------------------------- *
+ * Merges two sorted in ascending order portions of an array in to one sorted portion in ascending order.
+ *
+ * ARGUMENTS
+ * array: pointer to first element of an array
+ * tmpArray: pointer to first element of array used to store temparary values for algorithm
+ * start: index of the lower boundary where the sorting takes effect in the array
+ * middle:index of the boundary value between the two separete sorted portions of the array.
+ * end: index of the top boundary where the sorting takes effect in the array
+ *
+ * RETURN
+ * array sorted from array[start] to array[end] in ascending order.
+ * --------------------------------------------------------------------*/
+static void merge(int* array, int* tmpArray, size_t start, size_t middle, size_t end ){
 
     size_t i = start, j = middle, k = 0;
-    while(i <= middle && j<= end){// and k<end-start ??
+    while(i <= middle && j<= end){
         if(array[i] <= array[j] && (i < middle)){
-            tmp_array[k] = array[i];
+            tmpArray[k] = array[i];
             i++;
         }else{
-            tmp_array[k] = array[j];
+            tmpArray[k] = array[j];
             j++;
         }
         k++;
     }
 
-    for(; i<= middle ; k++, i++){
-        tmp_array[k] = array[i];
+    for(; i<= middle ; k++, i++){//cpy what is left from one of the portions into tmpArray
+        tmpArray[k] = array[i];
     }
     for(; j<= end ; k++, j++){
-        tmp_array[k] = array[j];
+        tmpArray[k] = array[j];
     }
-
 
     for(i = start, k = 0 ; i <= end; k++, i++){//cpy back into array from tmp array
-        array[i] = tmp_array[k];
+        array[i] = tmpArray[k];
     }
-
-    return;
+return;
 }
 
-static void MergeSort(int* array,int* tmp_array, size_t start, size_t end){
+/* -------------------------------------------------------------------- *
+ * Sorting algorithm, ascending order
+ *
+ * ARGUMENTS
+ * array: pointer to first element of an array
+ * tmpArray: pointer to first element of array used to store temparary values for algorith
+ * start: index of the lower boundary where the sorting takes effect in the array
+ * end: index of the top boundary where the sorting takes effect in the array
+ *
+ * RETURN
+ * array sorted from array[start] to array[end]
+ * --------------------------------------------------------------------*/
+static void mergeSort(int* array,int* tmpArray, size_t start, size_t end){
     if(start < end){
         size_t middle = (start + end) / 2;
-        MergeSort(array, tmp_array, start, middle);
-        MergeSort(array, tmp_array, ++middle, end);
-        Merge(array, tmp_array, start, middle, end);
+        mergeSort(array, tmpArray, start, middle);
+        mergeSort(array, tmpArray, ++middle, end);
+        merge(array, tmpArray, start, middle, end);
     }
 return;
 }
@@ -50,11 +73,11 @@ void sort(int* array, size_t length){
     if (!array)
         return;
 
-    int* tmp_array = malloc(length*sizeof(int));
-    if(!tmp_array)
+    int* tmpArray =(int*)malloc(length*sizeof(int));
+    if(!tmpArray)
         return;
         
-    MergeSort(array, tmp_array, 0 ,--length);
-    free(tmp_array);
-    return;
+    mergeSort(array, tmpArray, 0 ,--length);
+    free(tmpArray);
+return;
 }
